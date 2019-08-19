7006c3e2d912860716910e3123ec2a5adb6fc8b3
Austin Smith <austinsmith7531@gmail.com>
2019-08-19 18:03:04 -0400

    added the SMTP transporter

--- routes/contactForm.js
+++ routes/contactForm.js
@@ -3,10 +3,33 @@ const app = express();
 var router = express.Router();
 const axios = require("axios");
 const db = require("../models/contactUs");
+
 app.get("/api", function(req, res) {
   res.send("API home page");
 });
-app.post("/api/contact-us", (req, res) => {
-  contactForm(req, res);
-  console.log("submitted");
+
+router.post("/api/contact-me", (req, res, next) => {
+  var name = req.body.name;
+  var email = req.body.email;
+  var message = req.body.message;
+  var content = `name: ${name} \n email: ${email} \n message: ${content} `;
+
+  var mail = {
+    from: name,
+    to: "austinsmith7531@gmail.com",
+    subject: "New Message from Contact Form",
+    text: content
+  };
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        msg: "fail"
+      });
+    } else {
+      res.json({
+        msg: "success"
+      });
+    }
+  });
 });
---
+++ src/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  USER: "austinsmith7531@gmail.com",
+  PASS: "Zenziethewonderdog26!"
+};
